8b94bdf021012610dcfe71662982cbb94d695657
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-09-14 20:36:13 +0400

    api endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -31,6 +31,11 @@ JOBS =[
 @app.route("/")
 def hello_world():
   return render_template('home.html', jobs = JOBS)
+
+
+@app.route("/api/jobs")
+def ist_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
